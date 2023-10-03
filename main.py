0aba330c21bd0665bf34341420cf5c70a77f6ba6
SteamKatze <143835955+SteamKatze@users.noreply.github.com>
2023-10-03 16:49:47 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import re
-
-pattern_password = re.compile(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$')
-password = input("Enter your password. The password must contain one uppercase letter, one lowercase letter, one digit, one symbol, and have a length of 8 to 16 characters.: ")
-
-if pattern_password.match(password):
-    print("Valid password!")
-else:
-    print("Invalid password! The password must contain one uppercase letter, one lowercase letter, one digit, one symbol, and have a length of 8 to 16 characters.")
